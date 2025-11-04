27650aa8c17ce3206a2c1e90181f7cdc00fc313c
pakanati vishnuvardhan reddy <153382490+Vishnu5762@users.noreply.github.com>
2025-11-04 15:49:45 +0530

    Update Register.js

--- hms-frontend/src/pages/Register.js
+++ hms-frontend/src/pages/Register.js
@@ -37,7 +37,7 @@ const Register = () => {
         
         try {
             // 2. Submit to Backend
-            await axios.post("/register", payload);
+            await axios.post("api/auth/register", payload);
             
             setMessage("Registration successful! You can now log in.");
             
