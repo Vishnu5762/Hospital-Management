d478358e696516c7ec1f72c6232074c859a198e2
pakanati vishnuvardhan reddy <153382490+Vishnu5762@users.noreply.github.com>
2025-11-05 17:32:07 +0530

    Update CorsConfig.java

--- hms/src/main/java/com/example/hms/security/CorsConfig.java
+++ hms/src/main/java/com/example/hms/security/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.example.hms.config;
+package com.example.hms.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
